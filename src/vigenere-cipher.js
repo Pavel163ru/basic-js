0073d83d1ddb5d43e98ade3c3c6b4bd40cdc8ca6
Pavel R <work202006@gmail.com>
2021-03-03 10:50:52 +0400

    feat: implement vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,61 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  constructor(direct){
+    if(direct === undefined) direct = true;
+    this.direct = direct;
+  }
+  encrypt(message, key) {
+    if(!message || !key){
+      throw new Error();
+    }
+    let arr = message.toUpperCase().split('');
+    let shift = key.toUpperCase().split('').map(k => this.chars.indexOf(k));
+
+    let shiftIndex = 0;
+    let result = arr.map(c => {
+      let position = this.chars.indexOf(c);
+      if(position == -1){
+        return c;
+      }else{
+        let newpos = (position + shift[shiftIndex]) % this.chars.length;
+        shiftIndex = (shiftIndex + 1) % shift.length;
+        return this.chars.slice(newpos, newpos + 1);
+      }
+    });
+    
+    if(!this.direct){
+      result = result.reverse();
+    }
+
+    return result.join('');
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+    if(!message || !key){
+      throw new Error();
+    }
+    
+    let arr = message.toUpperCase().split('');
+    let shift = key.toUpperCase().split('').map(k => this.chars.indexOf(k));
+
+    let shiftIndex = 0;
+    let result = arr.map(c => {
+      let position = this.chars.indexOf(c);
+      if(position == -1){
+        return c;
+      }else{
+        let newpos = (this.chars.length + position - shift[shiftIndex]) % this.chars.length;
+        shiftIndex = (shiftIndex + 1) % shift.length;
+        return this.chars.slice(newpos, newpos + 1);
+      }
+    });
+
+    if(!this.direct){
+      result = result.reverse();
+    }
+    
+    return result.join('');
   }
 }
 
