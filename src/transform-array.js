fa0a32812cc0b2b72da83c4976c07de50cd00ddc
Pavel R <work202006@gmail.com>
2021-03-03 09:42:36 +0400

    feat: implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,47 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  arr = arr.slice();
+  let prevValue;
+  let currentValue;
+  for(let i = 0; i < arr.length; i++){
+    prevValue = currentValue;
+    currentValue = arr[i];
+    if(i == 0 && ['--discard-prev', '--double-prev'].includes(arr[i])){
+      arr.splice(i, 1);
+      i--;
+      continue;
+    }
+    if(i == (arr.length - 1) && ['--double-next', '--double-next'].includes(arr[i])){
+      arr.splice(i, 1);
+      continue;
+    }
+    switch(arr[i]){
+      case '--discard-next':
+        arr.splice(i, 2);
+        i--;
+        break;
+      case '--discard-prev':
+        if(prevValue == '--discard-next'){
+          arr.splice(i, 1);
+          i--;
+        }else{
+          arr.splice(i - 1, 2);
+          i -= 2;
+        }
+        break;
+      case '--double-next':
+        arr.splice(i, 1, arr[i + 1]);
+        break;
+      case '--double-prev':
+        if(prevValue == '--discard-next'){
+          arr.splice(i, 1);
+          i--;
+        }else{
+          arr.splice(i, 1, arr[i - 1]);
+        }
+        break;
+    }
+  }
+  return arr;
 };
